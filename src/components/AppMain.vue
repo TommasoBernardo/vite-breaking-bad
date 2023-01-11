28461853c9253d2be5e32a49a15f23e294444d89
TommasoBernardo <tommasob1210@gmail.com>
2023-01-11 19:38:45 +0100

    finish AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,42 @@
+<template>
+    <main class="m-3">
+        <div class="container my-background">
+            <div class="row g-4">
+                <div class="col-12 bg-dark">
+                    <p class="fs-5  text-white">Found {{ store.cardsList.length }} cards</p>
+                </div>
+                <div class="col-6 col-md-3 col-lg-2 d-flex" v-for=" card in store.cardsList">
+                    <div class="card text-bg-white mb-3">
+                        <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
+                        <div>
+                            <h6 class=" text-center">{{ card.name }}</h6>
+                            <p class="text-center">{{ card.archetype }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<script>
+import { store } from '../store.js';
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            store
+        };
+    },
+
+}
+</script>
+<style lang="scss" scoped>
+main {
+    height: calc(100% - 80px);
+
+}
+
+.my-background{
+    background-color: orange;
+}
+</style>
