aa3559ea7b407c3ef655cddf68e15ccac4292b69
TommasoBernardo <tommasob1210@gmail.com>
2023-01-11 19:39:32 +0100

    finish App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader />
+  <AppMain />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import { store } from './store.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store,
+    }
+  },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+<style lang="scss">
+@use './styles/partials/general.scss' as *;
+@use 'bootstrap/scss/bootstrap.scss' as *;
 </style>
