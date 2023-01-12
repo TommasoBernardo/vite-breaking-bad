f36031541fae0b03dbb7f430696a922b4a3bebcf
TommasoBernardo <tommasob1210@gmail.com>
2023-01-12 18:23:18 +0100

    update methods

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,6 +6,8 @@
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Yu-Gi-Oh%21_%28Logo%29.jpg/1200px-Yu-Gi-Oh%21_%28Logo%29.jpg"
                         alt="" class="img-fluid"> Yu-Gi-Oh Api</a>
             </div>
+
+            <TypeSelect  @search ="searchCharacter()"/>
         </nav>
     </header>
 </template>
@@ -21,23 +23,33 @@ export default {
     data() {
         return {
             store,
-            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=900",
+            apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?",
         }
     },
     methods: {
-        getCard() {
-            axios.get(this.apiUrl, {
-                params: {
-                }
-            })
-                .then((response) => {
-                    this.store.cardsList = (response.data.data);
-                })
+    getCards(archetypeInput = 'chaos') {
+      axios.get(this.apiUrl, {
+        params: {
+        num: 12,
+        offset: 0,
+        archetype: archetypeInput,
+        type: 'Effect Monster'
         }
+    })
+        .then((response) => {
+        this.store.cardsList = (response.data.data);
+        })
+        .catch(function (error) {
+        console.error(error);
+        })
     },
-    created() {
-        this.getCard();
+    getResponse() {
+    console.log('connesione riuscita!')
     }
+  },
+  created() {
+    this.getCards();
+  }
 }
 </script>
 <style lang="scss" scoped>
